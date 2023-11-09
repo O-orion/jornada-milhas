1f954c6547fd4d0f3106e00f6faf0bc0ae3ac8d0
Luca Viana <78035167+O-orion@users.noreply.github.com>
2023-11-09 11:40:49 -0300

    Implementando Interceptors

--- src/app/core/erro/erros.interceptor.ts
+++ src/app/core/erro/erros.interceptor.ts
@@ -3,16 +3,42 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable()
 export class ErrosInterceptor implements HttpInterceptor {
 
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<HttpErrorResponse>, next: HttpHandler): Observable<HttpEvent<HttpErrorResponse>> {
+
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+
+        let errorMessage = 'Ocorreu um erro desconhecido';
+
+        // error ocorrendo do lado do cliente
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erro do cliente: ${error.error.message}`
+
+        } else if (error.status === 404) {
+          errorMessage = 'Recurso não encontrado!'
+
+        } else if (error.status === 500) {
+          errorMessage = 'Error interno do servidor!'
+          
+        } else if (error.status === 401) {
+          errorMessage = 'Permissão negada! Você não possui autorização para acessar este recurso.'
+        }
+
+        console.error(error)
+        console.log(errorMessage)
+
+        return throwError(() => new Error('Ops, ocorreu um erro!'))
+      })
+    );
   }
 }
